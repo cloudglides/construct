c225f3d8a2b0e6ba94181e087c92b16c28c9d124
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-12-30 21:02:48 +0000

    Add currency calculation code

---
+++ src/lib/currency.ts
@@ -0,0 +1,58 @@
+import {
+	BASE_SHOP_SCORE_PER_HOUR,
+	BETA_DATE_CUTOFF,
+	BETA_MULTIPLIER,
+	BRICKS_PER_HOUR,
+	CLAY_PER_HOUR,
+	PRINT_MINUTES_PER_GRAM
+} from './defs';
+
+export function minutesToClay(minutes: number) {
+	const hours = minutes / 60;
+
+	return hours * CLAY_PER_HOUR;
+}
+
+export function minutesToBricks(minutes: number) {
+	const hours = minutes / 60;
+
+	return hours * BRICKS_PER_HOUR;
+}
+
+export function calculateShopScore(minutes: number, multiplier: number) {
+	const hours = minutes / 60;
+
+	return hours * multiplier * BASE_SHOP_SCORE_PER_HOUR;
+}
+
+export function calculateMinutes(timeSpent: number, printGrams: number) {
+	return timeSpent - printGrams * PRINT_MINUTES_PER_GRAM;
+}
+
+export function calculateCurrencyPayout(
+	minutes: number,
+	hasBasePrinter: boolean,
+	dateCreated: Date
+) {
+	return dateCreated.getTime() < BETA_DATE_CUTOFF.getTime()
+		? { clay: 0, bricks: minutesToBricks(minutes) * BETA_MULTIPLIER }
+		: hasBasePrinter
+			? { clay: 0, bricks: minutesToBricks(minutes) }
+			: { clay: minutesToClay(minutes), bricks: 0 };
+}
+
+export function calculatePayouts(
+	timeSpent: number,
+	printGrams: number,
+	shopScoreMultiplier: number,
+	hasBasePrinter: boolean,
+	dateCreated: Date
+) {
+	const time = calculateMinutes(timeSpent, printGrams);
+	const currency = calculateCurrencyPayout(time, hasBasePrinter, dateCreated);
+	const shopScore =
+		calculateShopScore(time, shopScoreMultiplier) *
+		(dateCreated.getTime() < BETA_DATE_CUTOFF.getTime() ? BETA_MULTIPLIER : 1);
+
+	return { ...currency, shopScore };
+}
